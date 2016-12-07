324cf95e9d59a7bdcbb41c34318cd6d0c5ab223e
breno-helf <breno.moura@hotmail.com>
2016-12-06 22:30:14 -0200

    Começou

---
+++ parte3/asm.c
@@ -0,0 +1,19 @@
+/*
+    Breno Helfstein Moura       NUSP: 9790972
+    Lucas Daher                 NUSP: ?
+    Raphael dos Reis Gusmao     NUSP: 9778561
+*/
+#include<stdio.h>
+#include<stlib.h>
+#include "asm.h"
+#include "error.h"
+#include "mactypes.h"
+#include "asmtypes.h"
+#include "optable.h"
+#include "opcodes.h"
+
+/*
+  Implementar a função
+*/
+
+int assemble(const char *filename, FILE *input, FILE *output);
---
+++ parte3/asm.h
@@ -0,0 +1,9 @@
+/*
+    Breno Helfstein Moura       NUSP: 9790972
+    Lucas Daher                 NUSP: ?
+    Raphael dos Reis Gusmao     NUSP: 9778561
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+int assemble(const char *filename, FILE *input, FILE *output);
