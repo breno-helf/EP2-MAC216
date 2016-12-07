a392bb166c536c1690b9fd7ac72c92c340686fdd
breno-helf <breno.moura@hotmail.com>
2016-12-07 01:30:11 -0200

    Trabalhando no começo

--- parte3/asm.c
+++ parte3/asm.c
@@ -5,6 +5,7 @@
 */
 #include<stdio.h>
 #include<stlib.h>
+#include "stable.h"
 #include "asm.h"
 #include "error.h"
 #include "mactypes.h"
@@ -16,4 +17,33 @@
   Implementar a função
 */
 
-int assemble(const char *filename, FILE *input, FILE *output);
+int assemble(const char *filename, FILE *input, FILE *output) {
+	SymbolTable alias_table, label_table, extern_table;
+	InsertionResult res;
+	alias_table = stable_create();
+	label_table = stable_create();
+	extern_table = stable_create();
+	res = stable_insert(alias_table, rA);
+	res->data->s = 255
+	res = stable_insert(alias_table, rR);
+	res->data->s = 254;
+	res = stable_insert(alias_table, rSP);
+	res->data->s = 253;
+	res = stable_insert(alias_table, rX);
+	res->data->s = 252;
+	res = stable_insert(alias_table, rY);
+	res->data->s = 251;
+	res = stable_insert(alias_table, rZ);
+	res->data->s = 250;
+
+	/*
+	  Usar o parser para fazer a lista ligada de instruções
+	  Lembre-se de sempre atualizar as tabelas de simbolo
+	  acima quando necessário.
+	*/
+
+	/*
+	  Após isso percorra a lista gerando o código objeto de cada
+	  instrução, definido por nós mesmos :P
+	*/
+}
---
+++ parte3/macas.c
@@ -0,0 +1,55 @@
+/*
+    Breno Helfstein Moura       NUSP: 9790972
+    Lucas Daher                 NUSP: 8991769
+    Raphael dos Reis Gusmao     NUSP: 9778561
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "asm.h"
+#include "error.h"
+
+const char out_suff[] = ".maco";
+
+/*
+  Dado o nome do arquivo de entrada gera
+  o nome do arquivo de saída
+*/
+
+char* outfile_name(char *name) {
+	int s, i = 0, tam;
+	char *out;
+	tam = strlen(name);
+	out = malloc(sizeof(char)*(tam + 10));	
+	while(i < tam && name[i] != '.') {
+		out[i] = name[i];
+		i++;
+	}
+	s = i;
+	tam = strlen(out_suff);
+	for(i = s; i < (s + tam); i++)
+		out[i] = out_suff[i - s];
+	
+	return out;
+}
+
+
+int main(int argc, char *argv[]) {
+	int i;
+	FILE *in, *out;
+	char *infile, *outfile;
+	
+	for(i = 1; i < argc; i++) {
+		infile = argv[i];
+		outfile = outfile_name(infile);
+		in = fopen(infile, "r");
+		out = fopen(outfile, "w");
+		/*
+		  Aqui é para fazer a chamada da função assemble.
+		  Descomente essa linha quando a função estiver pronta
+		  if(!assemble(infile, in, out)) 
+		      die("error");
+		*/
+	}
+	return 0;
+}
