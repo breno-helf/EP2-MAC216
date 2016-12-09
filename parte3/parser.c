21a9b6465f68e3aa4fa8f04afcbaed3ecfe47276
Raphael R. Gusmão <raphael.rgusmao@gmail.com>
2016-12-09 00:16:31 -0200

    Como arruma??2

--- parte3/parser.c
+++ parte3/parser.c
@@ -35,17 +35,18 @@ int check_rotulo (char *stg, const char *errptr)
     return 1;
 }
 
-char* HexToDec(char *s) {
+char* HexToDec (char *s)
+{
 	char *ret;
 	int num = 0, tam = 0, i, pow = 1, aux;
 	ret = malloc(15);
-	while(s[tam] != 0) tam++;
-	
-	for(i = tam - 1; i > 0; i--) {
-		
-		if(isdigit(s[i]))
+	while (s[tam] != 0) tam++;
+
+	for (i = tam - 1; i > 0; i--) {
+
+		if (isdigit(s[i]))
 			num += pow*(s[i] - '0');
-		else if(islower(s[i]))
+		else if (islower(s[i]))
 			num += pow*(s[i] - 'a' + 10);
 		else
 			num += pow*(s[i] - 'A' + 10);
@@ -53,12 +54,12 @@ char* HexToDec(char *s) {
 	}
 	tam = 0;
 	aux = num;
-	while(aux) {
+	while (aux) {
 		aux /= 10;
 		tam++;
 	}
 
-	for(i = tam - 1; i >= 0; i--) {
+	for (i = tam - 1; i >= 0; i--) {
 		ret[i] = '0' + (num%10);
 		num /= 10;
 	}
@@ -66,7 +67,7 @@ char* HexToDec(char *s) {
 }
 
 
-int parse(const char *s, SymbolTable alias_table, Instruction **instr, const char **errptr) {
+int parse (const char *s, SymbolTable alias_table, Instruction **instr, const char **errptr) {
     char *rotulo, *operador, *operand, **opd_read;
     const Operator *op;
 	Operand *opd[3];
@@ -83,6 +84,7 @@ int parse(const char *s, SymbolTable alias_table, Instruction **instr, const cha
         rotulo[j] = s[i];
         j++; i++;
     }
+
     if (j == 16) {
         set_error_msg("Rotulo invalido");
         *errptr = rotulo;
@@ -106,11 +108,12 @@ int parse(const char *s, SymbolTable alias_table, Instruction **instr, const cha
         *errptr = rotulo;
         return 0;
     }
-	
+
     (*instr)->label = rotulo;
     operador = malloc(sizeof(char) * 16);
     for (; isspace(s[i]); i++);
     j = 0;
+
     while (!(isspace(s[i])) && s[i] != '\0') {
         operador[j] = s[i];
         j++; i++;
@@ -118,9 +121,7 @@ int parse(const char *s, SymbolTable alias_table, Instruction **instr, const cha
 
 	if ((op = optable_find(operador)) != 0) {
         (*instr)->op = op;
-	}
-
-	else {
+	} else {
 		set_error_msg("Nao eh operador");
 		*errptr = operador;
 		return 0;
@@ -128,36 +129,34 @@ int parse(const char *s, SymbolTable alias_table, Instruction **instr, const cha
 
  operando:
 	for (; isspace(s[i]); i++);
+
     if ((*instr)->op->opd_types[0] != OP_NONE) {
 		EntryData *col;
 		j = 0;
+
         while (!(isspace(s[i])) && s[i] != '\0' && s[i] != ',') {
             opd_read[0][j] = s[i];
             j++; i++;
         }
-		col = stable_find(alias_table, opd_read[0]);
-		if(col) {
-			(*instr)->opds[0] = col->opd;
-		}
-        else if (((*instr)->op->opd_types[0]&LABEL) == LABEL) {
-        	(*instr)->opds[0] = operand_create_label(opd_read[0]);
 
+		col = stable_find(alias_table, opd_read[0]);
+		if (col) {
+			(*instr)->opds[0] = col->opd;
+		} else if (((*instr)->op->opd_types[0]&LABEL) == LABEL) {
+        	(*instr)->opds[0] = operand_create_label(opd_read[0]);
         } else if (!(isdigit(opd_read[0][0]) || opd_read[0][0] == '#') && ((*instr)->op->opd_types[0]&REGISTER) == REGISTER) {
 			int k = 0;
 			while(!(isdigit(opd_read[0][k]))) k++;
             (*instr)->opds[0] = operand_create_register(atoi(&opd_read[0][k]));
-
         } else if (((*instr)->op->opd_types[0]&STRING) == STRING) {
         	(*instr)->opds[0] = operand_create_string(opd_read[0]);
-
         } else {
-			if(opd_read[0][0] == '#')
+			if (opd_read[0][0] == '#')
 				opd_read[0] = HexToDec(opd_read[0]);
-			
         	(*instr)->opds[0] = operand_create_number(atoi(&opd_read[0][0]));
-
         }
     }
+
     if ((*instr)->op->opd_types[1] != OP_NONE) {
 		EntryData *col;
 	    for (; isspace(s[i]) || s[i] == ','; i++);
@@ -167,59 +166,56 @@ int parse(const char *s, SymbolTable alias_table, Instruction **instr, const cha
             j++; i++;
         }
 		col = stable_find(alias_table, opd_read[1]);
-		if(col) {
+		if (col) {
 			(*instr)->opds[1] = col->opd;
 		}
         else if (((*instr)->op->opd_types[1]&LABEL) == LABEL) {
         	(*instr)->opds[1] = operand_create_label(opd_read[1]);
-
         } else if (!(isdigit(opd_read[1][0]) || opd_read[1][0] == '#') && ((*instr)->op->opd_types[1]&REGISTER) == REGISTER) {
 			int k = 0;
 			while(!(isdigit(opd_read[1][k]))) k++;
             (*instr)->opds[1] = operand_create_register(atoi(&opd_read[1][k]));
-
         } else if (((*instr)->op->opd_types[1]&STRING) == STRING) {
         	(*instr)->opds[1] = operand_create_string(opd_read[1]);
-
         } else {
 			if(opd_read[1][0] == '#')
 				opd_read[1] = HexToDec(opd_read[1]);
-
         	(*instr)->opds[1] = operand_create_number(atoi(&opd_read[1][0]));
-
         }
     }
+
     if ((*instr)->op->opd_types[2] != OP_NONE) {
 		EntryData *col;
 	    for (; isspace(s[i]) || s[i] == ','; i++);
+
 	    j = 0;
         while (!(isspace(s[i])) && s[i] != '\0' && s[i] != ',') {
             opd_read[2][j] = s[i];
             j++; i++;
         }
+
 		col = stable_find(alias_table, opd_read[2]);
-		if(col) {
+		if (col) {
 			(*instr)->opds[2] = col->opd;
-		}
-        else if (((*instr)->op->opd_types[2]&LABEL) == LABEL) {
+		} else if (((*instr)->op->opd_types[2]&LABEL) == LABEL) {
         	(*instr)->opds[2] = operand_create_label(opd_read[2]);
-
         } else if (!(isdigit(opd_read[2][0]) || opd_read[2][0] == '#') && ((*instr)->op->opd_types[2]&REGISTER) == REGISTER) {
+            printf("*1\n");/*--------------------------------------------TODO */
 			int k = 0;
-			while(!(isdigit(opd_read[2][k]))) k++;
+			while(!(isdigit(opd_read[2][k]))) {
+                k++;
+            }
+            printf("*2\n");/*--------------------------------------------TODO */
             (*instr)->opds[2] = operand_create_register(atoi(&opd_read[2][k]));
-
         } else if (((*instr)->op->opd_types[2]&STRING) == STRING) {
         	(*instr)->opds[2] = operand_create_string(opd_read[2]);
-
         } else {
 			if(opd_read[2][0] == '#')
 				opd_read[2] = HexToDec(opd_read[2]);
-
         	(*instr)->opds[2] = operand_create_number(atoi(&opd_read[2][0]));
-
         }
     }
+
 	/*
     for (; s[i] != '\0' || s[i] != '*'; i++)
         if (!(isspace(s[i])) || s[i] != '\n') {
@@ -228,5 +224,6 @@ int parse(const char *s, SymbolTable alias_table, Instruction **instr, const cha
             return 0;
         }
 	*/
+    
     return 1;
 }
