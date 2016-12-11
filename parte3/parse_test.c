c335f402cdb5f9a16eac57a39b0d49eeb8b2b904
Raphael R. Gusmão <raphael.rgusmao@gmail.com>
2016-12-10 23:04:24 -0200

    Parser pronto 2

--- parte3/parse_test.c
+++ parte3/parse_test.c
@@ -68,59 +68,63 @@ int main (int argc, char *argv[])
 	line = 0;
 	aux = read_line(in, buffer);
 	while (aux != -1) {
-		if (aux > 0) {
-			const char *errptr;
-			Instruction *instr;
-			line++;
-			if (parse(buffer->data, alias_table, &instr, &errptr)) {
-				/* Line */
-				printf("line     = %s\n", buffer->data);
-				if (instr != NULL) {
-					/* IS */
-					if (instr->op->opcode == IS) {
-						if(chk_rotulo(instr->label, errptr)) {
-							Operand *opd = operand_create_register(instr->opds[0]->value.reg);
-							InsertionResult res = stable_insert(alias_table, instr->label);
-							res.data->opd = opd;
-						} else {
-							printf("line     %d: %s\n", line, buffer->data);
-							printf("^\n");
-							print_error_msg(NULL);
-							exit(1);
-						}
-					}
-					/* Label */
-					if (instr->label != NULL)
-						printf("label    = \"%s\"\n", instr->label);
-					else
-						printf("label    = n/a\n");
-					/* Operator */
-					printf("operator = %s\n", instr->op->name);
-					/* Operands */
-					printf("operands = ");
-					for (int i = 0; i < 3; i++) {
-						if (instr->opds[i]) {
-							if (i != 0) printf(", ");
-							if ((instr->opds[i]->type & LABEL) == LABEL)
-								printf("Label(\"%s\")", instr->opds[i]->value.label);
-							else if ((instr->opds[i]->type & STRING) == STRING)
-								printf("String(\"%s\")", instr->opds[i]->value.str);
-							else if ((instr->opds[i]->type & REGISTER) == REGISTER)
-								printf("Register(%u)", instr->opds[i]->value.reg);
-							else
-								printf("Number(%lld)", instr->opds[i]->value.num);
-						}
-					}
-					printf("\n\n");
-				} else printf("label = n/a\noperator = n/a\noperands = n/a\n\n");
-			} else {
-				printf("line %d: %s\n", line, buffer->data);
-				printf("^\n");
-				print_error_msg(NULL);
-				exit(1);
-			}
-		}
-		aux = read_line(in, buffer);
+        if (aux > 0) {
+    		const char *errptr;
+    		Instruction *instr = malloc(sizeof(Instruction));
+    		line++;
+    		if (parse(buffer->data, alias_table, &instr, &errptr)) {
+    			/* Line */
+    			printf("line     = %s\n", buffer->data);
+    			if (instr != NULL) {
+    				/* IS */
+    				if (instr->op->opcode == IS) {
+    					if(checkRotulo(instr->label, errptr)) {
+    						Operand *opd = operand_create_register(instr->opds[0]->value.reg);
+    						InsertionResult res = stable_insert(alias_table, instr->label);
+    						res.data->opd = opd;
+    					} else {
+    						printf("line     %d: %s\n", line, buffer->data);
+    						printf("^\n");
+    						print_error_msg(NULL);
+    						exit(1);
+    					}
+    				}
+    				/* Label */
+    				if (instr->label != NULL)
+    					printf("label    = \"%s\"\n", instr->label);
+    				else
+    					printf("label    = n/a\n");
+    				/* Operator */
+    				printf("operator = %s\n", instr->op->name);
+    				/* Operands */
+    				printf("operands = ");
+    				for (int i = 0; i < 3; i++) {
+    					if (instr->opds[i]) {
+    						if (i != 0) printf(", ");
+                            if ((instr->opds[i]->type & LABEL) == LABEL)
+                                printf("Label(\"%s\")", instr->opds[i]->value.label);
+                            else if ((instr->opds[i]->type & STRING) == STRING)
+                                printf("String(\"%s\")", instr->opds[i]->value.str);
+                            else if ((instr->opds[i]->type & REGISTER) == REGISTER)
+                                printf("Register(%u)", instr->opds[i]->value.reg);
+    						else
+                                printf("Number(%lld)", instr->opds[i]->value.num);
+    					}
+    				}
+    				printf("\n\n");
+    			} else printf("label = n/a\noperator = n/a\noperands = n/a\n\n");
+    		} else {
+    			printf("line %d: %s\n", line, buffer->data);
+    			printf("        ", errptr, *errptr);
+                errptr = &(buffer->data[8]);
+                for (char *p = &(buffer->data); p != errptr; p++) printf(" ");
+                printf("^\n");
+                printf("p:%p, *p:%d\n\n\n", errptr, *errptr);
+    			print_error_msg(NULL);
+    			exit(1);
+    		}
+        }
+        aux = read_line(in, buffer);
 	}
 
 	buffer_destroy(buffer);
--- parte3/parser.c
+++ parte3/parser.c
@@ -1,3 +1,8 @@
+/*
+    Breno Helfstein Moura       NUSP: 9790972
+    Lucas Daher                 NUSP: 8991769
+    Raphael dos Reis Gusmao     NUSP: 9778561
+*/
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
@@ -7,26 +12,23 @@
 #include "error.h"
 #include "stable.h"
 #include "optable.h"
-/*
-    Checa se o caracter eh letra, numero ou _
-*/
-int check_carac (char c)
+
+int isCharValid (char c)
 {
-    return ((c > 64 && c < 91) || (c == 95) || (c > 96 && c < 123) || (c > 47 && c < 58));
+    return ((c >= 'A' && c <= 'Z') || (c == '_') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'));
 }
 
-int check_rotulo (char *stg, const char *errptr)
+int checkRotulo (char *str, const char *errptr)
 {
-    int i = 1, j;
-    if (!(isalpha(stg[0]) || stg[0] == 95)) {
-        errptr = &stg[0];
+    int i = 1;
+    if (!(isalpha(str[0]) || str[0] == '_')) {
+        errptr = &str[0];
 		set_error_msg("primeiro char nao eh letra ou underscore");
 		return 0;
     }
-    while (stg[i] != '\0') {
-        j = check_carac(stg[i]);
-        if (!j) {
-			errptr = &stg[i];
+    while (str[i] != '\0') {
+        if (!isCharValid(str[i])) {
+			errptr = &str[i];
 			set_error_msg("Encontrado char que nao eh numero, letra ou underscore");
 			return 0;
         }
@@ -39,11 +41,9 @@ char* HexToDec (char *s)
 {
 	char *ret;
 	int num = 0, tam = 0, i, pow = 1, aux;
-	ret = malloc(15);
+	ret = emalloc(15);
 	while (s[tam] != 0) tam++;
-
 	for (i = tam - 1; i > 0; i--) {
-
 		if (isdigit(s[i]))
 			num += pow*(s[i] - '0');
 		else if (islower(s[i]))
@@ -58,7 +58,6 @@ char* HexToDec (char *s)
 		aux /= 10;
 		tam++;
 	}
-
 	for (i = tam - 1; i >= 0; i--) {
 		ret[i] = '0' + (num%10);
 		num /= 10;
@@ -66,164 +65,136 @@ char* HexToDec (char *s)
 	return ret;
 }
 
-
 int parse (const char *s, SymbolTable alias_table, Instruction **instr, const char **errptr) {
-    char *rotulo, *operador, *operand, **opd_read;
+    char *rotulo, *operador, **opd_read;
+    int i, j, opnum;
     const Operator *op;
-	Operand *opd[3];
-    int i, j, a;
-	j = 0;
-	(*instr) = malloc(sizeof(Instruction));
-  	opd_read = malloc(3 * sizeof(char *));
-	for (i = 0; i < 3; i++)
-		opd_read[i] = malloc(sizeof(char) * 16);
+    EntryData *data;
 
-    rotulo = malloc(sizeof(char) * 16);
+    /* Guarda em rotulo a primeira palavra encontrada */
+    rotulo = emalloc(sizeof(char) * 16);
     for (i = 0; isspace(s[i]); i++);
+    j = 0;
     while (!(isspace(s[i])) && s[i] != '\0' && j < 16) {
         rotulo[j] = s[i];
         j++; i++;
     }
+    rotulo[j] = '\0';
 
+    /* Verifica se o rotulo tem tamanho valido */
     if (j == 16) {
         set_error_msg("Rotulo invalido");
         *errptr = rotulo;
         return 0;
     }
 
-    rotulo[j] = '\0';
-    if (check_rotulo(rotulo, *errptr) == 0) {
+    /* Verifica se o rotulo eh valido */
+    if (!checkRotulo(rotulo, *errptr)) {
 		set_error_msg("Rotulo invalido");
         return 0;
     }
 
-	if ((op = optable_find(rotulo)) != 0) {
-        (*instr)->op = op; /* A funcao optable_find ja faz isso */
-		rotulo = NULL;
-        goto operando;
-	}
-
-	if (stable_find(alias_table, rotulo) != NULL) {
-        set_error_msg("Rotulo ja existe");
-        *errptr = rotulo;
-        return 0;
-    }
-
-    (*instr)->label = rotulo;
-    operador = malloc(sizeof(char) * 16);
-    for (; isspace(s[i]); i++);
-    j = 0;
-
-    while (!(isspace(s[i])) && s[i] != '\0') {
-        operador[j] = s[i];
-        j++; i++;
-    }
-
-	if ((op = optable_find(operador)) != 0) {
+    /* Verifica se o rotulo eh na verdade um operador*/
+	if ((op = optable_find(rotulo))) {
         (*instr)->op = op;
+		rotulo = NULL;
 	} else {
-		set_error_msg("Nao eh operador");
-		*errptr = operador;
-		return 0;
-	}
-
- operando:
-	for (; isspace(s[i]); i++);
-
-    if ((*instr)->op->opd_types[0] != OP_NONE) {
-		EntryData *col;
-		j = 0;
-
-        while (!(isspace(s[i])) && s[i] != '\0' && s[i] != ',') {
-            opd_read[0][j] = s[i];
-            j++; i++;
-        }
-
-		col = stable_find(alias_table, opd_read[0]);
-		if (col) {
-			(*instr)->opds[0] = col->opd;
-		} else if (((*instr)->op->opd_types[0]&LABEL) == LABEL) {
-        	(*instr)->opds[0] = operand_create_label(opd_read[0]);
-        } else if (!(isdigit(opd_read[0][0]) || opd_read[0][0] == '#') && ((*instr)->op->opd_types[0]&REGISTER) == REGISTER) {
-			int k = 0;
-			while(!(isdigit(opd_read[0][k]))) k++;
-            (*instr)->opds[0] = operand_create_register(atoi(&opd_read[0][k]));
-        } else if (((*instr)->op->opd_types[0]&STRING) == STRING) {
-        	(*instr)->opds[0] = operand_create_string(opd_read[0]);
-        } else {
-			if (opd_read[0][0] == '#')
-				opd_read[0] = HexToDec(opd_read[0]);
-        	(*instr)->opds[0] = operand_create_number(atoi(&opd_read[0][0]));
-        }
-    }
-
-    if ((*instr)->op->opd_types[1] != OP_NONE) {
-		EntryData *col;
-	    for (; isspace(s[i]) || s[i] == ','; i++);
-	    j = 0;
-        while (!(isspace(s[i])) && s[i] != '\0' && s[i] != ',') {
-            opd_read[1][j] = s[i];
-            j++; i++;
-        }
-		col = stable_find(alias_table, opd_read[1]);
-		if (col) {
-			(*instr)->opds[1] = col->opd;
-		}
-        else if (((*instr)->op->opd_types[1]&LABEL) == LABEL) {
-        	(*instr)->opds[1] = operand_create_label(opd_read[1]);
-        } else if (!(isdigit(opd_read[1][0]) || opd_read[1][0] == '#') && ((*instr)->op->opd_types[1]&REGISTER) == REGISTER) {
-			int k = 0;
-			while(!(isdigit(opd_read[1][k]))) k++;
-            (*instr)->opds[1] = operand_create_register(atoi(&opd_read[1][k]));
-        } else if (((*instr)->op->opd_types[1]&STRING) == STRING) {
-        	(*instr)->opds[1] = operand_create_string(opd_read[1]);
-        } else {
-			if(opd_read[1][0] == '#')
-				opd_read[1] = HexToDec(opd_read[1]);
-        	(*instr)->opds[1] = operand_create_number(atoi(&opd_read[1][0]));
-        }
-    }
-
-    if ((*instr)->op->opd_types[2] != OP_NONE) {
-		EntryData *col;
-	    for (; isspace(s[i]) || s[i] == ','; i++);
-
-	    j = 0;
-        while (!(isspace(s[i])) && s[i] != '\0' && s[i] != ',') {
-            opd_read[2][j] = s[i];
-            j++; i++;
-        }
-
-		col = stable_find(alias_table, opd_read[2]);
-		if (col) {
-			(*instr)->opds[2] = col->opd;
-		} else if (((*instr)->op->opd_types[2]&LABEL) == LABEL) {
-        	(*instr)->opds[2] = operand_create_label(opd_read[2]);
-        } else if (!(isdigit(opd_read[2][0]) || opd_read[2][0] == '#') && ((*instr)->op->opd_types[2]&REGISTER) == REGISTER) {
-            printf("*1\n");/*--------------------------------------------TODO */
-			int k = 0;
-			while(!(isdigit(opd_read[2][k]))) {
-                k++;
-            }
-            printf("*2\n");/*--------------------------------------------TODO */
-            (*instr)->opds[2] = operand_create_register(atoi(&opd_read[2][k]));
-        } else if (((*instr)->op->opd_types[2]&STRING) == STRING) {
-        	(*instr)->opds[2] = operand_create_string(opd_read[2]);
-        } else {
-			if(opd_read[2][0] == '#')
-				opd_read[2] = HexToDec(opd_read[2]);
-        	(*instr)->opds[2] = operand_create_number(atoi(&opd_read[2][0]));
-        }
-    }
-
-	/*
-    for (; s[i] != '\0' || s[i] != '*'; i++)
-        if (!(isspace(s[i])) || s[i] != '\n') {
-            set_error_msg("Erro de sintaxe");
-            *errptr = &(s[i]);
+        /* Verifica se o rotulo ja existe */
+    	if (stable_find(alias_table, rotulo) != NULL) {
+            set_error_msg("Rotulo ja existe");
+            *errptr = rotulo;
             return 0;
         }
-	*/
-    
+
+        /* Rotulo valido*/
+        (*instr)->label = rotulo;
+
+        /* Guarda em operador a proxima palavra encontrada */
+        operador = emalloc(sizeof(char) * 16);
+        for (; isspace(s[i]); i++);
+        j = 0;
+        while (!(isspace(s[i])) && s[i] != '\0') {
+            operador[j] = s[i];
+            j++; i++;
+        }
+        operador[j] = '\0';
+
+        /* Verifica se eh um operador */
+    	if (!(op = optable_find(operador))) {
+            set_error_msg("Nao eh operador");
+    		*errptr = operador;
+    		return 0;
+    	}
+
+        /* Operador valido*/
+        (*instr)->op = op;
+    }
+
+    /* Percorre os possiveis operandos da instrucao */
+    opd_read = emalloc(3 * sizeof(char *));
+    for (opnum = 0; opnum < 3; opnum++) {
+        if ((*instr)->op->opd_types[opnum] != OP_NONE) {
+            /* Guarda em opd_read[opnum] a proxima palavra encontrada */
+            opd_read[opnum] = emalloc(sizeof(char) * 16);
+            if (!opnum) for (; isspace(s[i]); i++);
+            else for (; isspace(s[i]) || s[i] == ','; i++);
+            j = 0;
+            while (!(isspace(s[i])) && s[i] != '\0' && s[i] != ',' && j < 16) {
+                opd_read[opnum][j] = s[i];
+                j++; i++;
+            }
+            opd_read[opnum][j] = '\0';
+
+            /* Verifica se o operando tem tamanho valido */
+            if (j == 0) {
+                set_error_msg("Operando faltando");
+                *errptr = &opd_read[opnum];
+                return 0;
+            }
+
+            /* Verifica se o operando tem tamanho valido */
+            if (j == 16) {
+                set_error_msg("Operando invalido");
+                *errptr = &opd_read[opnum];
+                return 0;
+            }
+
+            /* Verifica se o operando esta na tabela de alias */
+            data = stable_find(alias_table, opd_read[opnum]);
+
+            /* Se o operando lido for um alias */
+            if (data) {
+                (*instr)->opds[opnum] = data->opd;
+            }
+            /* Se o operando esperado for um label */
+            else if (((*instr)->op->opd_types[opnum] & LABEL) == LABEL) {
+                (*instr)->opds[opnum] = operand_create_label(opd_read[opnum]);
+            }
+            /* Se o operando for um registrador*/
+            else if (((*instr)->op->opd_types[opnum] & REGISTER) == REGISTER) {
+                if(!(isdigit(opd_read[opnum][0]) || opd_read[opnum][0] == '#')) {
+                    int k = 0;
+                    while(!isdigit(opd_read[opnum][k]) && k < 16) k++;
+                    if (k == 16) {
+                        set_error_msg("Operando invalido");
+                        *errptr = &opd_read[opnum];
+                        return 0;
+                    }
+                    (*instr)->opds[opnum] = operand_create_register(atoi(&opd_read[opnum][k]));
+                }
+            }
+            /* Se o operando esperado for uma string */
+            else if (((*instr)->op->opd_types[opnum] & STRING) == STRING) {
+                (*instr)->opds[opnum] = operand_create_string(opd_read[opnum]);
+            }
+            /* Se o operando for um numero hexadecimal */
+            else {
+                if (opd_read[opnum][0] == '#')
+                    opd_read[opnum] = HexToDec(opd_read[opnum]);
+                (*instr)->opds[opnum] = operand_create_number(atoi(&opd_read[opnum][0]));
+            }
+        }
+    }
+
     return 1;
 }
